48f0caf479a8feac75e3527b6c1a414e98f4eb4b
baya <baya@andatra.kiev.ua>
2019-03-10 20:11:58 +0200

    ! listen & process TCP:22 by ELB

--- main.tf
+++ main.tf
@@ -11,17 +11,17 @@ resource "aws_elb" "web-elb" {
   availability_zones = ["${split(",", var.availability_zones)}"]
 
   listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
+    instance_port     = 22
+    instance_protocol = "tcp"
+    lb_port           = 22
+    lb_protocol       = "tcp"
   }
 
   health_check {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 3
-    target              = "HTTP:80/"
+    target              = "TCP:22"
     interval            = 30
   }
 
